0aa2f5baeeea7ce278e55e0b0a0131b8d57a0e68
monguri <mongry@gmail.com>
2018-03-24 16:04:47 +0900

    AnimNodeの基本的なメソッドのテンプレートを用意したところでコミット

--- FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Private/AnimNode_FullbodyIKPractice.cpp
+++ FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Private/AnimNode_FullbodyIKPractice.cpp
@@ -5,3 +5,28 @@
 #include "SceneManagement.h"
 #include "AnimInstanceInterface_FullbodyIK.h"
 #include "DrawDebugHelpers.h"
+
+FAnimNode_FullbodyIKPractice::FAnimNode_FullbodyIKPractice()
+	: Setting(nullptr)
+{
+}
+
+void FAnimNode_FullbodyIKPractice::Initialize_AnyThread(const FAnimationInitializeContext& Context)
+{
+	Super::Initialize_AnyThread(Context);
+}
+
+void FAnimNode_FullbodyIKPractice::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms)
+{
+	check(OutBoneTransforms.Num() == 0);
+
+	if (!Setting)
+	{
+		return;
+	}
+}
+
+bool FAnimNode_FullbodyIKPractice::IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones)
+{
+	return true;
+}
--- FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Public/AnimNode_FullbodyIKPractice.h
+++ FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Public/AnimNode_FullbodyIKPractice.h
@@ -13,4 +13,18 @@ USTRUCT(BlueprintInternalUseOnly)
 struct FULLBODYIK_API FAnimNode_FullbodyIKPractice : public FAnimNode_SkeletalControlBase
 {
 	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=IK)
+	UFullbodyIKSetting* Setting;
+
+	FAnimNode_FullbodyIKPractice();
+
+	// FAnimNode_Base interface
+	virtual void Initialize_AnyThread(const FAnimationInitializeContext& Context) override;
+	// End of FAnimNode_Base interface
+
+	// FAnimNode_SkeletalControlBase interface
+	virtual void EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms) override;
+	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones) override;
+	// End of FAnimNode_SkeletalControlBase interface
 };
