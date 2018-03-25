7577b23a091475442618e607e0b329e7308403b4
monguri <mongry@gmail.com>
2018-03-25 15:51:11 +0900

    FAnimNode_FullbodyIKPractice::Initialize_AnyThreadの実装がだいぶ進んだところでコミット

--- FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Private/AnimNode_FullbodyIKPractice.cpp
+++ FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Private/AnimNode_FullbodyIKPractice.cpp
@@ -6,6 +6,7 @@
 #include "AnimInstanceInterface_FullbodyIK.h"
 #include "DrawDebugHelpers.h"
 
+
 FAnimNode_FullbodyIKPractice::FAnimNode_FullbodyIKPractice()
 	: Setting(nullptr)
 {
@@ -14,6 +15,63 @@ FAnimNode_FullbodyIKPractice::FAnimNode_FullbodyIKPractice()
 void FAnimNode_FullbodyIKPractice::Initialize_AnyThread(const FAnimationInitializeContext& Context)
 {
 	Super::Initialize_AnyThread(Context);
+
+	const USkeletalMeshComponent* Mesh = Context.AnimInstanceProxy->GetSkelMeshComponent();
+
+	// UFullbodyIKSetting:SolversからSolverInternalsに情報をつめかえる
+	for (FName IkEndBoneName : IkEndBoneNames)
+	{
+		FName BoneName = IkEndBoneName;
+
+		// IkEndBoneNamesに入っているエフェクタのボーンだけでなく、その先祖のすべてのボーンに対してFFullbodyIKSolverの設定は存在しうる
+		while (true)
+		{
+			int32 BoneIndex = Mesh->GetBoneIndex(BoneName);
+			if (BoneIndex == INDEX_NONE || SolverInternals.Contains(BoneIndex))
+			{
+				break;
+			}
+
+			FSolverInternal& SolverInternal = SolverInternals.Add(BoneIndex, FSolverInternal());
+			BoneIndices.Add(BoneIndex);
+
+			FName ParentBoneName = Mesh->GetParentBone(BoneName);
+			int32 ParentBoneIndex = Mesh->GetBoneIndex(ParentBoneName);
+			FFullbodyIKSolver Solver = GetSolver(BoneName);
+
+			SolverInternal.BoneIndex = BoneIndex;
+			SolverInternal.ParentBoneIndex = ParentBoneIndex;
+			SolverInternal.BoneIndicesIndex = -1;
+			SolverInternal.bTranslation = Solver.bTranslation;
+			SolverInternal.X = Solver.X;
+			SolverInternal.Y = Solver.Y;
+			SolverInternal.Z = Solver.Z;
+
+			if (ParentBoneIndex >= 0)
+			{
+				if (!SolverTree.Contains(ParentBoneIndex))
+				{
+					SolverTree.Add(ParentBoneIndex, TArray<int32>());
+				}
+
+				SolverTree[ParentBoneIndex].Add(BoneIndex);
+			}
+			
+			BoneName = ParentBoneName;
+		}
+	}
+
+	BoneIndices.Sort();
+	BoneCount = BoneIndices.Num();
+	BoneAxisCount = BoneCount * FAnimNode_FullbodyIKPractice::AXIS_COUNT;
+
+	for (int32 i = 0; i < BoneCount; ++i)
+	{
+		int32 BoneIndex = BoneIndices[i];
+		SolverInternals[BoneIndex].BoneIndicesIndex = i;
+	}
+
+	// 行列のセットアップ
 }
 
 void FAnimNode_FullbodyIKPractice::EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms)
@@ -29,4 +87,22 @@ void FAnimNode_FullbodyIKPractice::EvaluateSkeletalControl_AnyThread(FComponentS
 bool FAnimNode_FullbodyIKPractice::IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones)
 {
 	return true;
+}
+
+FFullbodyIKSolver FAnimNode_FullbodyIKPractice::GetSolver(FName BoneName) const
+{
+	check(Setting != nullptr);
+
+	for (const FFullbodyIKSolver& Solver : Setting->Solvers)
+	{
+		if (Solver.BoneName == BoneName)
+		{
+			return Solver;
+		}
+	}
+
+	// 見つからなければデフォルト
+	FFullbodyIKSolver Solver;
+	Solver.BoneName = BoneName;
+	return Solver;
 }
--- FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Public/AnimNode_FullbodyIKPractice.h
+++ FullbodyIKDemo/Plugins/FullbodyIK/Source/FullbodyIK/Public/AnimNode_FullbodyIKPractice.h
@@ -15,7 +15,13 @@ struct FULLBODYIK_API FAnimNode_FullbodyIKPractice : public FAnimNode_SkeletalCo
 	GENERATED_USTRUCT_BODY()
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=IK)
+	TArray<FName> IkEndBoneNames;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=IK)
 	UFullbodyIKSetting* Setting;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=IK)
+	int32 EffectorCountMax;
 
 	FAnimNode_FullbodyIKPractice();
 
@@ -27,4 +33,37 @@ struct FULLBODYIK_API FAnimNode_FullbodyIKPractice : public FAnimNode_SkeletalCo
 	virtual void EvaluateSkeletalControl_AnyThread(FComponentSpacePoseContext& Output, TArray<FBoneTransform>& OutBoneTransforms) override;
 	virtual bool IsValidToEvaluate(const USkeleton* Skeleton, const FBoneContainer& RequiredBones) override;
 	// End of FAnimNode_SkeletalControlBase interface
+
+private:
+	struct FSolverInternal
+	{
+	public:
+		FSolverInternal()
+		{
+		}
+
+		int32 BoneIndex;
+		int32 ParentBoneIndex;
+		int32 BoneIndicesIndex;
+		FTransform LocalTransform;
+		FTransform ComponentTransform;
+		FTransform InitLocalTransform;
+		FTransform InitComponentTransform;
+		bool bTranslation;
+		bool bLimited;
+		FFullbodyIKSolverAxis X;
+		FFullbodyIKSolverAxis Y;
+		FFullbodyIKSolverAxis Z;
+	};
+
+	static const int32 AXIS_COUNT = 3;
+
+	TArray<int32> BoneIndices;
+	int32 BoneCount;
+	int32 BoneAxisCount;
+
+	TMap<int32, FSolverInternal> SolverInternals;
+	TMap<int32, TArray<int32>> SolverTree;
+
+	FFullbodyIKSolver GetSolver(FName BoneName) const;
 };
